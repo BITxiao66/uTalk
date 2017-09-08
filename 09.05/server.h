14dd43ed0d96d24468a018a38a40aa7d24fa8d60
BITxiao666 <xiaoziyuan@bit.edu.cn>
2017-09-08 17:26:15 +0800

    final

---
+++ 09.05/server.h
@@ -0,0 +1,29 @@
+/********************************************************************************
+ * Files         : server.h
+ * Description   : the head file of srever.c
+ * Author        : xiaoziyuan
+ * Last Modified :
+ ********************************************************************************/
+
+
+/********************************************************************************
+ * Description : Declaration of all global variables in main.c. (add all global variables appearing in main.c here)
+ * Author      : xinchengxin
+ * Date        : 2017.08.31
+ * Time        : 15:38:06
+ ********************************************************************************/
+ 
+ 
+ 
+ /********************************************************************************
+  * Description : Declaration of all functions in main.c. (add all function appearing in main.c here)
+  * Author      : xinchengxin
+  * Date        : 2017.08.31
+  * Time        : 15:38:06
+  ********************************************************************************/
+ //static void user_signin(GtkWidget* widget, gpointer data);
+ //static void user_signup(GtkWidget* widget, gpointer data);
+ //static void to_signin(GtkWidget *window);
+ //static void to_signup(GtkWidget *window);
+ //static void create_signup_window();
+ //static void create_signin_window();
